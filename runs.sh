4eb0ffe1e20b4f48297ad792094af3c89b2aa3dd
dongyaozhu <doz022@ucsd.edu>
2022-11-21 09:54:01 -0800

    .

---
+++ runs.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# cifar10 zca ipc 50
+python distill.py --dataset=CIFAR10 --ipc=50 --syn_steps=30 --expert_epochs=2 --max_start_epoch=20 --zca --lr_img=100 --lr_lr=1e-05 --lr_teacher=0.001 --buffer_path=t0 --data_path=.
+
+# cifar100 zca ipc 50
+python distill.py --dataset=CIFAR100 --ipc=50 --syn_steps=80 --expert_epochs=2 --max_start_epoch=40 --zca --lr_img=1000 --lr_lr=1e-05 --lr_teacher=0.01 --buffer_path=t0 --data_path=.
+
